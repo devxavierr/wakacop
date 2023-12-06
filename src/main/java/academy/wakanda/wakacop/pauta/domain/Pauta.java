5cff29bd06214dea160809ab1839ac40a465a760
devxavierr <devxavierr@gmail.com>
2023-12-06 17:10:43 -0300

    implementa fluxo repository pauta - metodo cadastraPauta - oK!

--- src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -1,7 +1,10 @@
 package academy.wakanda.wakacop.pauta.domain;
 
+import academy.wakanda.wakacop.pauta.application.api.NovaPautaRequest;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
@@ -10,6 +13,7 @@ import java.util.UUID;
 @Entity
 @ToString
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Pauta {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,5 +23,12 @@ public class Pauta {
     private String descricao;
     private UUID idAssociadoAutor;
     private LocalDateTime dataCriacao;
+
+    public Pauta(NovaPautaRequest novaPauta) {
+        this.titulo = novaPauta.getTitulo();
+        this.descricao = novaPauta.getDescricao();
+        this.idAssociadoAutor = novaPauta.getIdAssociadoAutor();
+        this.dataCriacao = LocalDateTime.now();
+    }
 
 }
